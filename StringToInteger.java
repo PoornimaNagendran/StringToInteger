4afd01dde02a6513aeebc5a0c7aa881fc7f53feb
PoornimaNagendran <PoornimaNagendran@users.noreply.github.com>
2016-07-24 20:31:23 +0530

    StringToInteger

---
+++ StringToInteger.java
@@ -0,0 +1,19 @@
+package poorni;
+import java.util.*;
+public class StringToInteger {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		@SuppressWarnings("resource")
+		Scanner s=new Scanner(System.in);
+		String n="46545";
+		System.out.println("Enter a number");
+		String name=s.nextLine();
+		int a=Integer.parseInt(name);
+		int b=Integer.parseInt(n);
+		System.out.println("The entered integer is "+b);
+		System.out.println("The entered integer is "+a);
+
+	}
+
+}
